87e07ec1f9e684d0cd2aaaba1788bb7bfadc5a01
Patrick Coxall <mr.coxall@mths.ca>
2021-04-29 02:34:39 +0000

    init

---
+++ userInput.go
@@ -0,0 +1,21 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program does addition 
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function does addition
+	var firstInteger int
+	var secondInteger int
+
+	fmt.Print("Enter the first integer to add: ")
+	fmt.Scanln(&firstInteger)
+	fmt.Print("Enter the second integer to add: ")
+	fmt.Scanln(&secondInteger)
+	fmt.Println(firstInteger, " + ", secondInteger, " = ", (firstInteger + secondInteger))
+	fmt.Println("\nDone.")
+}
