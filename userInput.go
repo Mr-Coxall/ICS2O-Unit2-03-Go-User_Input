408138309e6614badf24065ef209ea7df59d1b2b
Patrick Coxall <mr.coxall@mths.ca>
2021-05-02 15:53:25 +0000

    updated program

--- userInput.go
+++ userInput.go
@@ -9,22 +9,18 @@ import "fmt"
 
 func main() {
 	// This function does addition
-	var firstInteger int
-	var secondInteger int
-	var sum int
+	var firstName string
+	var age int
 
 	// input
-	fmt.Println("This program adds 2 numbers together.")
+	fmt.Println("This program gets a users name and age.")
 	fmt.Println()
-	fmt.Print("Enter the first integer to add: ")
-	fmt.Scanln(&firstInteger)
-	fmt.Print("Enter the second integer to add: ")
-	fmt.Scanln(&secondInteger)
-
-	// process
-	sum = firstInteger + secondInteger
+	fmt.Print("Enter your first name: ")
+	fmt.Scanln(&firstName)
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
 
 	// output
-	fmt.Println(firstInteger, " + ", secondInteger, " = ", sum)
-	fmt.Println("\nDone.")
+	fmt.Print("Your info is: " , firstName, ", age ", age, ".")
+	fmt.Println("\n\nDone.")
 }
